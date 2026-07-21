57ec3078ed82d2b857efa4cda7347d898f20454c
loki0b <contato@henriquegarcialeite.com>
2026-07-21 20:02:44 -0300

    feat: implement make_packet

--- src/rdt.c
+++ src/rdt.c
@@ -1,6 +1,24 @@
 #include "../include/rdt.h"
+#include "../include/defs.h"
 #include <netinet/in.h>
 #include <stdlib.h>
+
+struct rdt_packet make_packet(FILE* data) {  
+    struct rdt_packet pkt;
+
+    size_t bytes_read = fread(pkt.data, sizeof(char), BUFFER_SIZE, data);
+    if (bytes_read > 0) {
+        pkt.payload_size = bytes_read;
+        pkt.is_last_chunk = feof(data) ? 1 : 0;
+        pkt.checksum = make_checksum(pkt.data, bytes_read);
+    }
+
+    return pkt;
+}
+
+short make_checksum(unsigned char* buffer, size_t size) {
+    return 0;
+}
 
 void udt_send(const struct rdt_packet* pkt) {
     int bytes_read;
