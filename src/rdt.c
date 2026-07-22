2d5508b603ef5d82fbc703183281d1a73b083262
loki0b <contato@henriquegarcialeite.com>
2026-07-22 09:42:24 -0300

    fix: change argment size to properly send only the payload data

--- src/rdt.c
+++ src/rdt.c
@@ -37,7 +37,7 @@ unsigned short make_checksum(unsigned char* buffer, size_t size) {
 void udt_send(const struct rdt_packet* pkt) {
     int bytes_read;
 
-    bytes_read = sendto(socket_fd, pkt->data, BUFFER_SIZE, 0, (struct sockaddr*)&remote_host_addr, sizeof(remote_host_addr));
+    bytes_read = sendto(socket_fd, pkt->data, pkt->payload_size, 0, (struct sockaddr*)&remote_host_addr, sizeof(remote_host_addr));
     if (bytes_read < 0) {
         perror("(sendto)");
         exit(EXIT_FAILURE);
