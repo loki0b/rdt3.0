586586ef4a355e7babc18d58427bbd2d9bad75b6
loki0b <contato@henriquegarcialeite.com>
2026-07-21 22:58:43 -0300

    refact: change variable typles

--- include/rdt.h
+++ include/rdt.h
@@ -4,15 +4,24 @@
 
 // __attribute__((packed))
 struct rdt_packet {
-    short seq_num;
-    short ack_num;
-    short checksum;
-    short payload_size;
-    char  is_last_chunk;
+    unsigned char seq_num;
+    unsigned char ack_num;
+    unsigned short checksum;
+    unsigned short payload_size;
+    unsigned char  is_last_chunk;
     unsigned char data[BUFFER_SIZE];
 };
 
 void rdt_send(FILE* data, char* filename);
-struct rdt_packet make_packet(FILE* data);
-short make_checksum(unsigned char* buffer, size_t size);
 void udt_send(const struct rdt_packet*);
+void rdt_rcv(struct rdt_packet*);
+
+struct rdt_packet make_packet(FILE* data);
+unsigned short make_checksum(unsigned char* buffer, size_t size);
+
+int is_corrupt(struct rdt_packet* pkt);
+int has_ack(struct rdt_packet* pkt, unsigned char x);
+int has_seq(struct rdt_packet* pkt, unsigned char x);
+
+void extract(struct rdt_packet* pkt);
+void deliver_data();
