969d9f08bad14fa3bd100a25649354bfe8b60dac
loki0b <contato@henriquegarcialeite.com>
2026-07-24 02:16:34 -0300

    feat: implement common lib

---
+++ src/rdt_common.c
@@ -0,0 +1,132 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <errno.h>
+#include "../include/rdt_common.h"
+
+struct rdt_packet make_packet(FILE* data) {
+    struct rdt_packet pkt;
+    memset(&pkt, 0, sizeof(pkt));
+
+    if (data == NULL) {
+        pkt.payload_size = 0;
+    } else {
+        size_t bytes_read = fread(pkt.data, sizeof(char), PKT_PAYLOAD_SIZE, data);
+
+        if (ferror(data) != 0) {
+            perror("(fread)");
+            exit(EXIT_FAILURE);
+        }
+
+        pkt.payload_size = bytes_read;
+        pkt.is_last_chunk = feof(data) ? 1 : 0;
+    }
+
+    return pkt;
+}
+
+int rdt_rcv(struct rdt_packet* pkt) {
+    int bytes_to_read;
+    struct sockaddr_in receiver_addr;
+    socklen_t receiver_addr_len;
+
+    memset(pkt, 0, sizeof(*pkt));
+
+    if (start_timer()) {
+        fprintf(stderr, "(start_timer): failed");
+        exit(EXIT_FAILURE);
+    }
+
+    receiver_addr_len = sizeof(receiver_addr);
+    bytes_to_read = recvfrom(socket_fd, pkt, sizeof(struct rdt_packet), 0, (struct sockaddr*)&receiver_addr, &receiver_addr_len);
+    if (bytes_to_read < 0) {
+        // Timeout
+        if (errno == EAGAIN || errno == EWOULDBLOCK || errno == EINPROGRESS) return 1;
+        else {
+            perror("(recvfrom)");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if (stop_timer()) {
+        fprintf(stderr, "(start_timer): failed");
+        exit(EXIT_FAILURE);
+    }
+
+    return 0;
+}
+
+void udt_send(const struct rdt_packet* pkt) {
+    ssize_t bytes_to_read;
+    size_t bytes_to_send;
+
+    bytes_to_send = sizeof(struct rdt_packet) - PKT_PAYLOAD_SIZE + pkt->payload_size;
+    bytes_to_read = sendto(socket_fd, pkt, bytes_to_send, 0, (struct sockaddr*)&remote_host_addr, sizeof(remote_host_addr));
+    if (bytes_to_read < 0) {
+        perror("(sendto)");
+        exit(EXIT_FAILURE);
+    }
+}
+
+unsigned short make_checksum(const struct rdt_packet* pkt, size_t size) {
+    return 0;
+}
+
+// Validate checksum
+int is_corrupt(struct rdt_packet* pkt) {
+    if (pkt == NULL) return -1;
+    
+    size_t pkt_size = sizeof(struct rdt_packet) - PKT_PAYLOAD_SIZE + pkt->payload_size;
+    int ret = (pkt->checksum != make_checksum(pkt, pkt_size)) ? 1 : 0;
+
+    return ret;
+}
+
+// Check if ack_num is x
+int has_ack(struct rdt_packet* pkt, unsigned char x) {
+    if (pkt == NULL) return -1;
+
+    return (pkt->ack_num == x) ? 1 : 0;
+}
+
+// Check if seq_num is x
+int has_seq(struct rdt_packet* pkt, unsigned char x) {
+    if (pkt == NULL) return -1;
+
+    return (pkt->seq_num == x) ? 1 : 0;
+}
+
+// Set a timer for ACK response from receiver
+// default 500ms
+int start_timer() {
+    struct timeval time = {
+        .tv_sec = 0,
+        .tv_usec = 500000
+    };
+
+    if (setsockopt(socket_fd, SOL_SOCKET, SO_RCVTIMEO, &time, sizeof(time)) < 0) {
+        perror("(setsockopt)");
+        
+        return 1;
+    }
+
+    return 0;
+}
+
+// Stop the timer
+int stop_timer() {
+    struct timeval time = {
+        .tv_sec = 0,
+        .tv_usec = 0
+    };
+
+    if (setsockopt(socket_fd, SOL_SOCKET, SO_RCVTIMEO, &time, sizeof(time)) < 0) {
+        perror("(setsockopt)");
+        
+        return 1;
+    }
+
+    return 0;
+}
