665fa1a91a1ef3ad5cb93723b1b94afd7cd0b3e7
loki0b <contato@henriquegarcialeite.com>
2026-07-27 16:17:48 -0300

    feat: add logic to send the filename

--- src/rdt_sender.c
+++ src/rdt_sender.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include "../include/rdt_sender.h"
 
 enum RDT_SENDER_STATE {
@@ -7,20 +8,29 @@ enum RDT_SENDER_STATE {
     WAIT_ACK_1
 };
 
-int rdt_send(FILE* data) {
+int rdt_send(FILE* data, const char* filename) {
     if (data) {
         struct rdt_packet sndpkt;
         struct rdt_packet rcvpkt;
         enum RDT_SENDER_STATE state;
-        int is_the_last_ack_received, timeout;
+        int is_the_last_ack_received, timeout, is_fileheader_send;
         size_t pkt_size;
 
         state = WAIT_SEQ_0;
-        is_the_last_ack_received = timeout = 0;
+        is_the_last_ack_received = timeout = is_fileheader_send = 0;
         while (!is_the_last_ack_received) {
             switch (state) {
                 case WAIT_SEQ_0: {
-                    sndpkt = make_packet(data);
+                    if (!is_fileheader_send && filename != NULL) {
+                        sndpkt = make_packet(NULL);
+                        sndpkt.payload_size = strlen(filename);
+                        memcpy(sndpkt.data, filename, strlen(filename));
+                        sndpkt.data[strlen(filename)] = '\0';
+                        is_fileheader_send = 1;
+                    } else {
+                        sndpkt = make_packet(data);
+                    }
+                    
                     sndpkt.seq_num = 0;
                     pkt_size = sizeof(struct rdt_packet) - PKT_PAYLOAD_SIZE + sndpkt.payload_size;
                     sndpkt.checksum = make_checksum(&sndpkt, pkt_size);
